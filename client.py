3f69eacfcda31973af69b794c6d8c489d031a624
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-01 12:18:29 +0800

    Letak send and receive dalam satu function
    sebab nak kasi player wait for another player. Sebelum ni kalau same player hantar dua kali server anggap itu datang dari dua player

--- client.py
+++ client.py
@@ -10,18 +10,12 @@ try:
 except socket.error as e:
         print(str(e))
         
-def send():
+def send_recv():
     while True:
         Input = input("[1 for Rock] [ 2 for Paper] [3 for Scissors]")
         ClientSocket.send(Input.encode())
-        
-def recev():
-    while True:
+        print("Wait for opponent")
         message = ClientSocket.recv(1024).decode()
         print('Result: ' + message +'\n')
-        
-
-thread_send = threading.Thread(target=send)
-thread_receive = threading.Thread(target=recev)
-thread_send.start()
-thread_receive.start()
+thread_send_recv = threading.Thread(target=send_recv)
+thread_send_recv.start()
