0af40715e8082c9799127336d597f559d49e513e
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-04 00:39:45 +0800

    Update Rock Paper Scissor Server.py
    1. Tambah quit options
    2. Assign Ip address dekat output supaya senang nak tahu siapa menang
    3. Output sekali kedua-dua tangan pemain supaya boleh tahu siapa patutnya menang.
    4. Tambah comment supaya senang faham

--- Rock Paper Scissor Server.py
+++ Rock Paper Scissor Server.py
@@ -26,9 +26,12 @@ def accept():
            p = Process(target=process_start, args=(s_sock,))
            p.start()
 
-        else:
+        elif numplayer =='2':
            print("Double player mode")
            mula_listen(s_sock,s_addr[0])
+            
+        elif numplayer == '3':
+            s_sock.sendall(str.encode("Good game, Bye!"))#message if quit
 
 def mula_listen(s_sock,s_addr):
         #thread objects
@@ -39,29 +42,58 @@ def mula_listen(s_sock,s_addr):
 def listen_and_reply(s_sock,s_addr):
     while True:
         message = s_sock.recv(1024).decode()
-        listen_list.append(message)
-        player_list.append(s_addr)
-        send_list.append(s_sock)
+        if message == '4':
+            s_sock.sendall(str.encode("Good game, Bye!"))#message if quit
+        listen_list.append(message)#list for players' hand
+        player_list.append(s_addr)#list for player name
+        send_list.append(s_sock)#list of client to broadcast
+        print(player_list)
         if len(player_list) < 2:
              toall("Wait for opponent...")
         if len(listen_list) == 2:
-            playerA =listen_list[0]
-            playerB =listen_list[1]
-            if (playerA == '1' and playerB =='1') or (playerA == '2' and playerB =='2') or (playerA == '3' and playerB =='3'):
-                result = 'Draw'
-            elif(playerA == '1' and playerB =='2') or (playerA == '2' and playerB =='3') or (playerA == '3' and playerB =='1'):
-                result = f'{player_list[1]}WIN, {player_list[0]} LOSE'
-            elif (playerA == '1' and playerB =='3') or (playerA == '2' and playerB =='1') or (playerA == '3' and playerB =='2'):
-                result = f'{player_list[0]}WIN, {player_list[1]}B LOSE'
+           if listen_list[0]:
+                #To assign user input with the correct hand. 1 for rock...
+                if listen_list[0] == '1': 
+                    playerA = 'Rock'
+                elif listen_list[0] == '2':
+                    playerA = 'Paper'
+                elif listen_list[0] == '3':
+                    playerA = 'Scissors'
+            if listen_list[1]:
+                if listen_list[1] == '1':
+                    playerB = 'Rock'
+                elif listen_list[1] == '2':
+                    playerB = 'Paper'
+                elif listen_list[1] == '3':
+                    playerB = 'Scissors'
+            # ip address: hand
+            print(f'{player_list[0]} : {playerA}')
+            print(f'{player_list[1]} : {playerB}')
+            # Ip address : Hand VS Ip address 2 : Hand2
+            result= player_list[0] + ": "+ playerA + " VS " + player_list[1]+": "+playerB
             print(result)
-            toall(result)
+            #Decide who wins
+            if (playerA == 'Rock' and playerB =='Rock') or (playerA == 'Paper' and playerB =='Paper') or (playerA == 'Scissors' and playerB =='Scissors'):
+                fin_result = f'{result} \nResult: Draw' 
+                """
+                Ip address : Hand VS Ip address 2 : Hand2
+                Result: Draw
+                """
+            elif(playerA == 'Rock' and playerB =='Paper') or (playerA == 'Paper' and playerB =='Scissors') or (playerA == 'Scissors' and playerB =='Rock'):
+                fin_result = f'{result} \nResult:{player_list[1]} WIN!'
+            elif (playerA == 'Rock' and playerB =='Scissors') or (playerA == 'Paper' and playerB =='Rock') or (playerA == 'Scissors' and playerB =='Paper'):
+                fin_result = f'{result} \nResult:{player_list[0]} WIN!'
+            print(fin_result)
+            toall(fin_result)
             listen_list.clear()
+            player_list.clear()
             
 def process_start(s_sock):
     while True:
        choose = s_sock.recv(2048).decode()
-       com = random.choice(rps)
-       if choose == '1' and com =='Rock':
+       com = random.choice(rps)# Random func to decide what computer chooses
+        # to messy :(
+      """ if choose == '1' and com =='Rock':
             s_sock.sendall(str.encode(result1))
        elif choose == '1' and com =='Paper':
             s_sock.sendall(str.encode(result2))
@@ -78,9 +110,28 @@ def process_start(s_sock):
        elif choose == '3' and com =='Paper':
             s_sock.sendall(str.encode(result3))
        elif choose == '3' and com =='Scissors':
-            s_sock.sendall(str.encode(result1))
+            s_sock.sendall(str.encode(result1))"""
+       if choose== '1':
+             hand = 'Rock'
+       elif choose== '2':
+             hand = 'Paper'
+       elif choose== '3':
+             hand = 'Scissors'
        elif choose == '4' :
             s_sock.sendall(str.encode("Good game, Bye!"))
+            
+        # Ip address : Hand VS Computer : Hand2    
+       result= "Player: "+ hand + " VS  Computer: "+ com
+       if (hand == 'Rock' and com =='Rock') or (hand == 'Paper' and com =='Paper') or (hand == 'Scissors' and com =='Scissors'):
+                fin_result = f'{result} \nResult: Draw'
+                 """
+                Ip address : Hand VS Computer : Hand2
+                Result: Draw
+                """
+       elif(hand == 'Rock' and com =='Paper') or (hand == 'Paper' and com =='Scissors') or (hand == 'Scissors' and com =='Rock'):
+                fin_result = f'{result} \nResult: Computer WIN!'
+       elif (hand == 'Rock' and com =='Scissors') or (hand == 'Paper' and com =='Rock') or (hand == 'Scissors' and com =='Paper'):
+                fin_result = f'{result} \nResult:Player WIN!'
 
 
 def toall(message):
