04d3339aca8478275acf1fdcfcf490cff5232e74
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-04 00:15:43 +0800

    Update Rock Paper Scissor Client.py

--- Rock Paper Scissor Client.py
+++ Rock Paper Scissor Client.py
@@ -4,11 +4,13 @@ import time
 
 def send_recv():
     while True:
-        Input = input("[1 for Rock] [ 2 for Paper] [3 for Scissors]")
-        ClientSocket.send(Input.encode())
-        print("Wait for opponent")
-        message = ClientSocket.recv(1024).decode()
-        print('Result: ' + message +'\n')
+        Input = input("[1 for Rock] [ 2 for Paper] [3 for Scissors][4 to Quit game]")
+        if Input == '4':
+            quit()
+        elif Input:
+            ClientSocket.send(Input.encode())
+            message = ClientSocket.recv(1024).decode()
+            print(message+'\n')
 
 ClientSocket = socket.socket()
 host = '192.168.56.105'
@@ -19,15 +21,17 @@ try:
         print (" Welcome to Rock - Paper - Scissor game ")
         numplayer = input("Choose number of players. Single (1) or Double (2) players. \n Enter 1 or 2 : ")
         ClientSocket.send(numplayer.encode())
-        if numplayer =='1':
-                while True:
-                    choose=input('\n[1 for Rock] [ 2 for Paper] [3 for Scissors] [4 for Quit game]: ')
+        if numplayer =='3':
+            result = ClientSocket.recv(1024)
+            quit()
+        elif numplayer =='1':
+               while True:
+                    choose=input('\n[1 for Rock] [ 2 for Paper] [3 for Scissors][4 to Quit game]: ')
                     ClientSocket.send(str.encode(choose))
                     result = ClientSocket.recv(1024)
                     print(result.decode('utf-8'))
-                    if choose == '4' :
+                    if choose == '4':
                        quit()
-                        
         elif numplayer =='2':
                 thread_send_recv = threading.Thread(target=send_recv)
                 thread_send_recv.start()
