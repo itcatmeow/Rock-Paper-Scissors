d6b0dfc41d6d95700155418af278baa79b979d00
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-03 19:25:33 +0800

    Assign ip dekat client instead of player a win / player b win..

--- server.py
+++ server.py
@@ -6,7 +6,17 @@ import math
 from multiprocessing import Process
 import random
 import threading
+import random
 
+result1 = 'Draw'
+result2 = 'Lose'
+result3 = 'Win'
+result4 = 'Win'
+result5 = 'Draw'
+result6 = 'Lose'
+result7 = 'Lose'
+result8 = 'Win'
+result9 = 'Draw'
 result=''
 close = 'You Quit'
 startmsg = 'Lets Play'
@@ -14,7 +24,7 @@ msg1 = '1 Player chosen'
 rps = ["Rock", "Paper", "Scissors"]
 player_list = []
 listen_list = []
-
+send_list = []
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(("",8888))
 print("listening...")
@@ -22,32 +32,87 @@ def accept():
     while True:
         s.listen()
         s_sock,s_addr = s.accept()
-        player_list.append(s_sock)
-        mula_listen(s_sock)
-def mula_listen(s_sock):
+        numplayer = s_sock.recv(1024).decode()
+        print("Connected to :",s_addr[0])
+        if numplayer == '1' :
+           print("One player mode")
+           p = Process(target=process_start, args=(s_sock,))
+           p.start()
+
+        else:
+           print("Double player mode")
+           mula_listen(s_sock,s_addr[0])
+
+def mula_listen(s_sock,s_addr):
         #thread objects
-        client_thread = threading.Thread( target=listen_and_reply, args=(s_sock,))
+        client_thread = threading.Thread( target=listen_and_reply, args=(s_sock,s_addr))
         #the list of argument for the function
         client_thread.start()
-            
-def listen_and_reply(s_sock):
+
+def listen_and_reply(s_sock,s_addr):
     while True:
         message = s_sock.recv(1024).decode()
         listen_list.append(message)
+        player_list.append(s_addr)
+        send_list.append(s_sock)
         if len(listen_list) == 2:
             playerA =listen_list[0]
             playerB =listen_list[1]
             if (playerA == '1' and playerB =='1') or (playerA == '2' and playerB =='2') or (playerA == '3' and playerB =='3'):
                 result = 'Draw'
             elif(playerA == '1' and playerB =='2') or (playerA == '2' and playerB =='3') or (playerA == '3' and playerB =='1'):
-                result = 'Player A lOSE, Player B WIN'
+                result = f'{player_list[1]}WIN, {player_list[0]} LOSE'
             elif (playerA == '1' and playerB =='3') or (playerA == '2' and playerB =='1') or (playerA == '3' and playerB =='2'):
-                result = 'Player A WIN, Player B LOSE'
+                result = f'{player_list[0]}WIN, {player_list[1]}B LOSE'
             print(result)
             toall(result)
             listen_list.clear()
-            
+
+def process_start(s_sock):
+    while True:
+       choose = s_sock.recv(2048).decode()
+       com = random.choice(rps)
+       if choose == '1' and com =='Rock':
+            s_sock.sendall(str.encode(result1))
+       elif choose == '1' and com =='Paper':
+            s_sock.sendall(str.encode(result2))
+       elif choose == '1' and com =='Scissors':
+            s_sock.sendall(str.encode(result3))
+       elif choose == '2' and com =='Rock':
+            s_sock.sendall(str.encode(result4))
+       elif choose == '2' and com =='Paper':
+            s_sock.sendall(str.encode(result5))
+       elif choose == '2' and com =='Scissors':
+            s_sock.sendall(str.encode(result6))
+       elif choose == '3' and com =='Rock':
+            s_sock.sendall(str.encode(result7))
+       elif choose == '3' and com =='Paper':
+            s_sock.sendall(str.encode(result8))
+       elif choose == '3' and com =='Scissors':
+            s_sock.sendall(str.encode(result9))
+
+def listen_and_reply_single(s_sock):
+    while True:
+        message = s_sock.recv(1024).decode()
+        if not message:
+            print(f"Player has been disconnected: {s_addr[0]}")
+            return
+        listen_list.append(message)
+        com = random.choice(rps)
+        print("Com : ", com)
+        if len(listen_list) == 2:
+            player =listen_list[0]
+            if (player == '1' and com =='Rock') or (player == '2' and com =='Paper') or (player == '3' and com =='Scissors'):
+                result = 'Draw'
+            elif(player == '1' and com =='Paper') or (player == '2' and com =='Scissors') or (player == '3' and com =='Rock'):
+                result = 'You Lose'
+            elif (player == '1' and com =='Scissors') or (player == '2' and com =='Rock') or (player == '3' and com =='Paper'):
+                result = 'You Win'
+            print(result)
+            toall(result)
+            listen_list.clear()
+
 def toall(message):
-    for s_sock in player_list:
-        s_sock.send(message.encode())
+    for s_sock in send_list:
+        s_sock.send(str.encode(message))
 accept()
