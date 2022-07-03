bffe4b0fcc3e69c4a0a2c059826e34dbc98d6a96
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-07-03 20:55:16 +0800

    Update Rock Paper Scissor Client.py

--- Rock Paper Scissor Client.py
+++ Rock Paper Scissor Client.py
@@ -9,3 +9,25 @@ def send_recv():
         print("Wait for opponent")
         message = ClientSocket.recv(1024).decode()
         print('Result: ' + message +'\n')
+
+ClientSocket = socket.socket()
+host = '192.168.56.105'
+port = 8888
+print('Waiting for connection')
+try:
+        ClientSocket.connect((host,port))
+        print (" Welcome to Rock - Paper - Scissor game ")
+        numplayer = input("Choose number of players. Single (1) or Double (2) players. \n Enter 1 or 2 : ")
+        ClientSocket.send(numplayer.encode())
+        if numplayer =='1':
+                while True:
+                    choose=input('\n[1 for Rock] [ 2 for Paper] [3 for Scissors]: ')
+                    ClientSocket.send(str.encode(choose))
+                    result = ClientSocket.recv(1024)
+                    print(result.decode('utf-8'))
+        elif numplayer =='2':
+                thread_send_recv = threading.Thread(target=send_recv)
+                thread_send_recv.start()
+
+except socket.error as e:
+        print(str(e))
